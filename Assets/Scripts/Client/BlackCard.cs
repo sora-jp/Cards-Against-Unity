519e0c7e63bd5a7f6cbe6529888a5d44dbc8ac2c
Sora <oskar.tornevall@gmail.com>
2020-06-14 20:18:47 +0200

    Stuff

---
+++ Assets/Scripts/Client/BlackCard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BlackCard : MonoBehaviour
+{
+    public TextMeshProUGUI pickText;
+    public GameObject pickContainer;
+
+    Card m_card;
+
+    void Awake()
+    {
+        m_card = GetComponent<Card>();
+    }
+
+    void Update()
+    {
+        pickText.text = m_card.CardDef.Pick.ToString();
+        pickContainer.SetActive(m_card.CardDef.Pick > 1);
+    }
+}
